910ca51fbaaa83723881ab9ebd0f03273299b882
SaiSwetha5 <saiswetha.patnaik@cognizant.com>
2026-02-13 18:44:04 +0530

    Changes in angular.json

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,4 @@ import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
 import { msalInstance } from 'app/msal.config';
 
-(async () => {
-  try {
-      await msalInstance.initialize();
-    await bootstrapApplication(AppComponent, appConfig);
-  } catch (err) {
-    console.error(err);
-  }
-})();
-  
+(async () => {  await msalInstance.initialize();  await bootstrapApplication(AppComponent, appConfig);})().catch(err => console.error('App bootstrap failed', err));
