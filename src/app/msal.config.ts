c8ab49be7b78cfe908085123da425a4f128bc303
SaiSwetha5 <saiswetha.patnaik@cognizant.com>
2026-02-16 21:54:00 +0530

    config changes

--- src/app/msal.config.ts
+++ src/app/msal.config.ts
@@ -6,7 +6,10 @@ export const msalInstance = new PublicClientApplication({
   auth: {
     clientId: environment.msal.clientId,
     authority: environment.msal.authority,
-    redirectUri: window.location.origin,  
+   
+ redirectUri:  `${window.location.origin}/auth-callback`,
+    postLogoutRedirectUri: window.location.origin
+
   },
   cache: {
     cacheLocation: 'localStorage',
