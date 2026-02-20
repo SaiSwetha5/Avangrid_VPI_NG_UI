1f8de255a30963d3e68577455f39b631e1fa44d0
SaiSwetha5 <saiswetha.patnaik@cognizant.com>
2026-02-20 12:24:15 +0530

    ENV VAR changed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,13 @@
+const env = window.__env ?? {};
+
 export const environment = {
   production: true,
-  apiBaseUrl: '${API_BASE_URL}',
-  mUrl: '${GRAPH_API_URL}',
-  accessScope: '${BACKEND_SCOPE}',
+  apiBaseUrl: env["APIBASEURL"] || "",
+  graphApiUrl: env["GRAPHAPIURL"] || "",
+  accessScope: env["BACKENDSCOPE"] || "",
   msal: {
-    clientId: '${MSAL_CLIENT_ID}',
-    authority: 'https://login.microsoftonline.com',
-    tenantId: '${AZURE_TENANT_ID}'
+    clientId: env["MSALCLIENTID"] || "",
+    authority: "https://login.microsoftonline.com", 
+    tenantId: env["AZURETENANTID"] || ""
   }
 };
