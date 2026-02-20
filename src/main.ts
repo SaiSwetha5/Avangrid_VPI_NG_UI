960ef9e7bdfcc23ab08321d752e124178f2a9006
SaiSwetha5 <saiswetha.patnaik@cognizant.com>
2026-02-20 12:50:04 +0530

    SAmple

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,6 @@ import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
 import { msalInstance } from 'app/msal.config';
 
-(async () => {  await msalInstance.initialize();  await bootstrapApplication(AppComponent, appConfig);})().catch(err => console.error('App bootstrap failed', err));
+(async () => {  await msalInstance.initialize(); 
+  await bootstrapApplication(AppComponent, appConfig);
+})().catch(err => console.error('App bootstrap failed', err));
